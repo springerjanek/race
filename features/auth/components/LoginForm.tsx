351e552f75001a3580c1fc72bbf290069f4cd96d
springerjanek <springerjan2@gmail.com>
2026-02-25 11:05:10 +0100

    feat: loginForm component

---
+++ features/auth/components/LoginForm.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { useAuth } from "../hooks/useAuth";
+
+export const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const { login } = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (username.trim().length <= 0) {
+      toast("Please input username!!");
+      return;
+    }
+    await login(username);
+  };
+
+  return (
+    <form className="flex gap-3" onSubmit={handleSubmit}>
+      <Input
+        placeholder="Username"
+        value={username}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setUsername(e.target.value)
+        }
+      />
+      <Button type="submit">Enter</Button>
+    </form>
+  );
+};
