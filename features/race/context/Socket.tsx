3f6a228005db66e6f2d8e1d3e18ba3bd0a579641
springerjanek <springerjan2@gmail.com>
2026-02-25 13:42:25 +0100

    SocketContext

---
+++ features/race/context/Socket.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { io, Socket } from "socket.io-client";
+
+type SocketContextType = { socket: Socket | null };
+const SocketContext = createContext<SocketContextType>({ socket: null });
+
+export const SocketProvider = ({ children }: { children: ReactNode }) => {
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const s = io("http://localhost:3000");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
+
+export const useSocket = () => useContext(SocketContext);
