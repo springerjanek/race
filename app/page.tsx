1c0efc92f4d879f0f8696139091bae374650050d
springerjanek <springerjan2@gmail.com>
2026-02-25 11:04:46 +0100

    feat: Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import AnimatedTypingMotion from "@/components/shadcn-space/animated-text/animated-text-03";
+import { LoginForm } from "@/features/auth/components/LoginForm";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      Test
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-2 items-center">
+          <h1 className=" text-2xl">Welcome</h1>
+          <AnimatedTypingMotion />
+        </div>
+        <LoginForm />
+      </div>
     </div>
   );
 }
